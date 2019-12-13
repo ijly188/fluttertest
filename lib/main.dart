bf605aef513de0dbed1ff2adc770697e3630b932
terryyao <terry.yao.global@gmail.com>
2019-12-13 11:01:19 +0800

    finished one page with title, we use map to render the card

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,62 @@
-import 'package:flutter/material.dart';
-
+import 'package:flutter/material.dart';   //引入扁平化設計的組件
+/*
+main(){
+  runApp(MyApp());  // 運行下面定義的class
+}
+*/
+// 可以簡寫成這樣子用箭頭函式
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget{
+  final List<String> _products = ['food test', 'e8 e8 e8 e8 e8', 'aaaa'];     // 這個不知道在衝三小，但是簡單講就是加底線的變數就只能在這個東西裡面跑這樣
+  // 這個新的類別會覆蓋他繼承的類別（提示作用而已）
   @override
+  // 實作一個類別，他是繼承於剛剛引入的package中的stateless組建類別
   Widget build(BuildContext context) {
+    // 這個function將輸出內容
+    // context 將會有一個app的基本資訊
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times test:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      home : Scaffold(                              // home裡面是一個scaffold，白色背景主題
+        appBar : AppBar(                            // 增加一個bar
+          title : Text('我的App')                    // 增加bar的標題
+        ),                                         
+        body : Column(
+          children : [
+            Container(
+              margin : EdgeInsets.all(10.0),
+              child: RaisedButton(
+                onPressed: () {},                   //  尚未註冊點擊事件
+                child: Text('Add Product'),         //  按鈕文字
+              ),                                   
+            ),                                     
+            Column(                                 // Column
+              children : _products.map((element) => Card(       // 建立一張卡片
+                  child : Column(                               // 有一個欄位
+                    children: <Widget>[                         // 一個放了很多組建的陣列
+                      Image.asset('assets/food.jpg'), // 要這個設定的話，你要先去設定pubspec.yaml最下面去寫
+                      //Text('Food Paradise')           // 內容文字，會出現在圖片下方
+                      Text(element)
+                    ],                               
+                  )                                  
+                )                                    
+              ).toList(),
+            ),                                     
+            /*
+            Card(                                   // 建立一張卡片
+              child : Column(                       // 有一個欄位
+                children: <Widget>[                 // 一個放了很多組建的陣列
+                  Image.asset('assets/food.jpg'),   // 要這個設定的話，你要先去設定pubspec.yaml最下面去寫
+                  Text('Food Paradise')             // 內容文字，會出現在圖片下方
+                ],                                 
+              )                                    
+            )                                      
+            */
+          ]
+        )
+        
+      ),                                           
+    );                                             
+    // 最後輸出結果要return出去他才知道
+    // MaterialApp是整個扁平化app的最外層
   }
 }
